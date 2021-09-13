5dfe231970eb486a37ffe51be591ea41128c4ace
Davis Chloe <818834@stma.k12.mn.us>
2021-09-13 15:09:17 -0500

    yo

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Bye World!")
+
+#print first drink
+print("boba tea")
+
+#print first food
+print("chicken parmesan")
+
+#print first band
+print("BTS")
+
+#print second band
+print("NCT")
