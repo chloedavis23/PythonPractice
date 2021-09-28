e32b8df94516228746a994b102c2602dee0fba83
chloedavis23 <818834@mystma.org>
2021-09-28 14:28:05 -0500

    dictionary

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-def addTwoNumbas(x, y): 
-    print(x + y)
+def define(word): 
+    word.lower()
+    
+    if word == "Osowski": 
+        return "This word does not exist in this dictionary"
+        
+    elif word == "Skunk":
+        return "Stinky creature that farts"
 
-addTwoNumbas(10, 59)
+    elif word == "Computer": 
+        return "This word does not exist in this dictionary"
 
-def madLib(instrument, number, day, letter, major, num, integer, adj, adjective, fraction, adject, nameInstrument, adjecti, noun, familyMember, nameOfSong, adjecEd, favoriteArtistComposer): 
-    print("While I've stayed at home, I've practiced the " + instrument + " for " + number + " hours every " + day + ". My favorite key signature to play and practice in is " + letter + major + " It has " + num + " flats and " + integer + " sharps. I like this key signature because it is " + adjective + " and " + adj + ". I've also gotten awesome at counting time signatures. The time signature I love is " + fraction + ", because it is " + adject + " and sounds like a " + noun + ". My " + familyMember + " likes it when I play " + nameOfSong + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be " + adjecEd + " of how great I am at playing my instrument. My teacher might even think I sound like " + favoriteArtistComposer)
+    elif word == "Cloud": 
+        return "Floating water in the sky"
 
-madLib("flute", "69", "Sunday", "Z ", "minor.", "420", "69420", "apalling", "grueling", "5/4", "hysterical", "banjo", "critical", "chupacabra", "uncle", "Mikrokosmos", "fileted", "BTS")
+    elif word == "Fun": 
+        return "This word does not exist in this dictionary"
+
+    elif word == "ARMY": 
+        return "BTS fanbase"
+
+    elif word == "Music": 
+        return "Good stuff" 
+
+    elif word == "Student": 
+        return "Educated person in school"
+
+    elif word == "Deez": 
+        return "nutz" 
+        
+    else: 
+        return "That word is not in this dictionary YET."
+
+print(define("Deez"))
